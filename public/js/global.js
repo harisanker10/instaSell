b0433a2e9e209d07154a8bbc584b3ebed94cea62
hari sanker <harisanker10@gmail.com>
2023-11-22 17:03:26 +0530

    bug fixes

--- public/js/global.js
+++ public/js/global.js
@@ -1,141 +1,146 @@
-
-let locationInitialVal = '';
-document.addEventListener('DOMContentLoaded', async () => {
-
-    const setLocation = (event)=>{
-         
-        window.loadingOn();
-        const name = event.target.value;
-        if(name.trim() === ''){
-            locationBtn.value = locationInitialVal;
-            window.loadingOff();
-            return;
-        } 
-
-
-        let locationAddress = name.split(/[\s,]+/);
-        locationAddress = locationAddress.join('%20') + '%20india';
-
-        const url = `https://api.geoapify.com/v1/geocode/search?text=${locationAddress}&format=json&apiKey=5b8cbdeeff9f48a7be8492329512e1e8`
-
-        console.log(url)
-        fetch(url)
-            .then(res => res.json())
-            .then(data => {
-
-                const locationDetails = data.results[0]
-
-                console.log(locationDetails)
-
-                locationBtn.value = locationDetails.suburb || locationDetails?.city || locationDetails?.state;
-                localStorage.setItem('locationDetails', JSON.stringify(locationDetails));
-                window.location.reload()
-
-                if (locationBtn.value.includes('undefined')) locationBtn.value = '';
-
-            })
-            .catch(err => {
-                console.log(err);
-                locationBtn.value = '';
-                window.notify("Try a different location")
-            })
-    }
-
-    const locationBtn = document.querySelector('#location-btn');
-
-    locationBtn.addEventListener('blur', (event) => {
-       
-        setLocation(event);
-
-
-    })
-    locationBtn.addEventListener('keypress', (event) => {
-        if(event.key === 'Enter'){
-            event.preventDefault()
-            setLocation(event);
-        }
-
-
-    })
-
-    locationBtn.addEventListener('click', (event) => {
-        locationInitialVal = event.target.value;
-        locationBtn.value = '';
-    })
-
-
-    let locationDetails = JSON.parse(localStorage.getItem('locationDetails'));
-    if (locationDetails) {
-        locationBtn.value = locationDetails?.suburb || locationDetails?.city || locationDetails?.state;
-        return;
-    } else {
-
-        navigator.geolocation.getCurrentPosition((position) => {
+try {
+    
+    let locationInitialVal = '';
+    document.addEventListener('DOMContentLoaded', async () => {
+    
+        const setLocation = (event)=>{
+             
             window.loadingOn();
-            const { latitude, longitude } = position.coords;
-            const url = `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&apiKey=5b8cbdeeff9f48a7be8492329512e1e8`;
-
-
-
-
+            const name = event.target.value;
+            if(name.trim() === ''){
+                locationBtn.value = locationInitialVal;
+                window.loadingOff();
+                return;
+            } 
+    
+    
+            let locationAddress = name.split(/[\s,]+/);
+            locationAddress = locationAddress.join('%20') + '%20india';
+    
+            const url = `https://api.geoapify.com/v1/geocode/search?text=${locationAddress}&format=json&apiKey=5b8cbdeeff9f48a7be8492329512e1e8`
+    
+            console.log(url)
             fetch(url)
-                .then((res) => res.json())
-                .then((data) => {
-                    locationDetails = data.features[0].properties;
-                    if (data.features[0].properties.suburb)
-                        locationBtn.value = data.features[0].properties.suburb;
-                    else
-                        locationBtn.value = data.features[0].properties.city;
+                .then(res => res.json())
+                .then(data => {
+    
+                    const locationDetails = data.results[0]
+    
+                    console.log(locationDetails)
+    
+                    locationBtn.value = locationDetails.suburb || locationDetails?.city || locationDetails?.state;
                     localStorage.setItem('locationDetails', JSON.stringify(locationDetails));
                     window.location.reload()
-                    
-                    
+    
+                    if (locationBtn.value.includes('undefined')) locationBtn.value = '';
+    
                 })
-                .catch((error) => {
-                    console.error('Fetch error:', error);
-                    window.location.reload()
-                });
-
-
-        });
-
-
-
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
-
-const onClickBtns = document.querySelectorAll('.on-click-btn');
-onClickBtns.forEach(btn => {
-    btn.addEventListener('click', (event) => {
-        const link = btn.getAttribute('navLink');
-        if (link) {
-            event.preventDefault();
-            window.location.href = btn.getAttribute('navLink');
+                .catch(err => {
+                    console.log(err);
+                    locationBtn.value = '';
+                    window.notify("Try a different location")
+                })
         }
+    
+        const locationBtn = document.querySelector('#location-btn');
+    
+        locationBtn.addEventListener('blur', (event) => {
+           
+            setLocation(event);
+    
+    
+        })
+        locationBtn.addEventListener('keypress', (event) => {
+            if(event.key === 'Enter'){
+                event.preventDefault()
+                setLocation(event);
+            }
+    
+    
+        })
+    
+        locationBtn.addEventListener('click', (event) => {
+            locationInitialVal = event.target.value;
+            locationBtn.value = '';
+        })
+    
+    
+        let locationDetails = JSON.parse(localStorage.getItem('locationDetails'));
+        if (locationDetails) {
+            locationBtn.value = locationDetails?.suburb || locationDetails?.city || locationDetails?.state;
+            return;
+        } else {
+    
+            navigator.geolocation.getCurrentPosition((position) => {
+                window.loadingOn();
+                const { latitude, longitude } = position.coords;
+                const url = `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&apiKey=5b8cbdeeff9f48a7be8492329512e1e8`;
+    
+    
+    
+    
+                fetch(url)
+                    .then((res) => res.json())
+                    .then((data) => {
+                        locationDetails = data.features[0].properties;
+                        if (data.features[0].properties.suburb)
+                            locationBtn.value = data.features[0].properties.suburb;
+                        else
+                            locationBtn.value = data.features[0].properties.city;
+                        localStorage.setItem('locationDetails', JSON.stringify(locationDetails));
+                        window.location.reload()
+                        
+                        
+                    })
+                    .catch((error) => {
+                        console.error('Fetch error:', error);
+                        window.location.reload()
+                    });
+    
+    
+            });
+    
+    
+    
+    
+        }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     })
-})
-
-
-
-
-
-
+    
+    const onClickBtns = document.querySelectorAll('.on-click-btn');
+    onClickBtns.forEach(btn => {
+        btn.addEventListener('click', (event) => {
+            const link = btn.getAttribute('navLink');
+            if (link) {
+                event.preventDefault();
+                window.location.href = btn.getAttribute('navLink');
+            }
+        })
+    })
+    
+    
+    
+    
+    
+    
+    
+}catch(err){
+    console.log(err)
+}
--- public/js/globalFunctions.js
+++ public/js/globalFunctions.js
@@ -1,96 +1,101 @@
-const notify = (message, time, err) => {
-    const notificationBody = document.querySelector('.notification');
-    const notificationCloseBtn = document.querySelector('.notification-close');
-    const content = document.querySelector('.notification-content');
-
-    notificationBody.style.transition = "200ms";
-
-    notificationBody.classList.remove('hide');
-    if (err) {
-        if (err.includes('err')) notificationBody.style.backgroundColor = 'red';
-        else notificationBody.style.backgroundColor = err;
-    }
-    notificationCloseBtn.addEventListener('click', () => {
-        // Remove transition effect before hiding
-        notificationBody.style.transition = "none";
-        notificationBody.classList.add('hide');
-        notificationBody.style.opacity = "0%";
-        notificationBody.style.transform = "";
-    });
-
-
-
-    content.innerHTML = message;
-
-    void notificationBody.offsetWidth;
-
-    notificationBody.style.opacity = "100%";
-    notificationBody.style.transform = "translateY(5rem)";
-
-    if (!time) {
-
-        setTimeout(() => {
-
-            notificationCloseBtn.click()
-        }, 7000);
-
-
-    }
-    if (time && time != 'persist') {
-
-        setTimeout(() => {
-
-            notificationCloseBtn.click()
-        }, time);
-
-
-    }
-
-};
-
-
-document.addEventListener('DOMContentLoaded', (event) => {
-
-
-    const notifyCookie = document.cookie
-        .split('; ')
-        .find((cookie) => cookie.startsWith('notify='));
-
-    if (notifyCookie) {
-        const [, notifyValue] = notifyCookie.split('=');
-        window.notify(decodeURIComponent(notifyValue), 5000)
-        console.log('Notify:', decodeURIComponent(notifyValue));
-        document.cookie = 'notify=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-    }
-})
-
-const loadingOverlay = document.querySelector('#loading-overlay')
-const loadingSlider = document.querySelector('#loading-slider')
-
-const loadingOn = () => {
-    loadingOverlay.classList.remove('hide');
-    loadingSlider.classList.remove('hide');
-
-}
-
-const loadingOff = () => {
-    loadingOverlay.classList.add('hide');
-    loadingSlider.classList.add('hide')
-}
-
-const loadingSliderOn = ()=>{
-    loadingSlider.classList.remove('hide')
+try {
+    const notify = (message, time, err) => {
+        const notificationBody = document.querySelector('.notification');
+        const notificationCloseBtn = document.querySelector('.notification-close');
+        const content = document.querySelector('.notification-content');
     
+        notificationBody.style.transition = "200ms";
+    
+        notificationBody.classList.remove('hide');
+        if (err) {
+            if (err.includes('err')) notificationBody.style.backgroundColor = 'red';
+            else notificationBody.style.backgroundColor = err;
+        }
+        notificationCloseBtn.addEventListener('click', () => {
+            // Remove transition effect before hiding
+            notificationBody.style.transition = "none";
+            notificationBody.classList.add('hide');
+            notificationBody.style.opacity = "0%";
+            notificationBody.style.transform = "";
+        });
+    
+    
+    
+        content.innerHTML = message;
+    
+        void notificationBody.offsetWidth;
+    
+        notificationBody.style.opacity = "100%";
+        notificationBody.style.transform = "translateY(5rem)";
+    
+        if (!time) {
+    
+            setTimeout(() => {
+    
+                notificationCloseBtn.click()
+            }, 7000);
+    
+    
+        }
+        if (time && time != 'persist') {
+    
+            setTimeout(() => {
+    
+                notificationCloseBtn.click()
+            }, time);
+    
+    
+        }
+    
+    };
+    
+    
+    document.addEventListener('DOMContentLoaded', (event) => {
+    
+    
+        const notifyCookie = document.cookie
+            .split('; ')
+            .find((cookie) => cookie.startsWith('notify='));
+    
+        if (notifyCookie) {
+            const [, notifyValue] = notifyCookie.split('=');
+            window.notify(decodeURIComponent(notifyValue), 5000)
+            console.log('Notify:', decodeURIComponent(notifyValue));
+            document.cookie = 'notify=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+        }
+    })
+    
+    const loadingOverlay = document.querySelector('#loading-overlay')
+    const loadingSlider = document.querySelector('#loading-slider')
+    
+    const loadingOn = () => {
+        loadingOverlay.classList.remove('hide');
+        loadingSlider.classList.remove('hide');
+    
+    }
+    
+    const loadingOff = () => {
+        loadingOverlay.classList.add('hide');
+        loadingSlider.classList.add('hide')
+    }
+    
+    const loadingSliderOn = ()=>{
+        loadingSlider.classList.remove('hide')
+        
+    }
+    
+    window.loadingOn = loadingOn;
+    window.loadingOff = loadingOff;
+    window.loadingSliderOn = loadingSliderOn;
+    
+    
+    
+    
+    
+    
+    
+    window.notify = notify;
+    
+}catch(err){
+    console.log(err)
 }
-
-window.loadingOn = loadingOn;
-window.loadingOff = loadingOff;
-window.loadingSliderOn = loadingSliderOn;
-
-
-
-
-
-
-
-window.notify = notify;
